2e8cb11bdffc92eeb5cfced98a154b58c920a2a1
Coby J <addictedtops2@gmail.com>
2018-03-09 21:33:14 +1000

    Pre-Ethan fix push
    Fixed filesystem issue caused by Ethan's GitHub fuck up (was totally github :3) but no seriously, its fixed and i'll be pushing his file-changes in a sec

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-# source2-mod
-
-A Source 2 SDK Base being developed by ethan101au, Msalinas2877, OzxyBox and Demez
-(and others from the Source 2 Research Center Discord server).
-
-If you'd like to contribute you can do so here on GitHub or come collaborate with us
-on our Source 2 Research Center Discord server: https://discord.gg/rDPjZMz
-=======
 A modified version of Source 2 using the Dota 2, Performance Test and Robot Repair content.
 [Join the Discord!](https://discord.gg/rDPjZMz)
 
@@ -17,4 +8,3 @@ Launch dota2cfg.exe
   
 # Game
 Launch using run_client.bat or run_server.bat
->>>>>>> 3e945aff62e9bc95bc78e36bb66b68f578945a57
---
+++ game/vr/scripts/vscripts/map_scripts/vr_prefab_spawn_test.lua
@@ -0,0 +1,110 @@
+------------ Copyright (c) Valve Corporation, All rights reserved. ------------
+--
+-- Controls the gameplay logic for map: vr_aperture_main
+--
+-------------------------------------------------------------------------------
+
+ScriptLogicEntity = nil
+ScriptLogicScope = nil
+HasUpdateFunc = false
+
+-----------------------------------------------------------
+function VRAperturePrint( msg )
+	print( "VR_PREFAB_SPAWN_TEST: " .. msg )
+end
+
+
+-----------------------------------------------------------
+function OnInit()
+	VRAperturePrint( "OnInit()" )
+
+	--Create the logic_script entity that controls the logic for the map
+	ScriptLogicEntity = SpawnScriptEnt( "PrefabSpawnTestLogic", "vr_prefab_spawn_test_logic" )
+	ScriptLogicScope = ScriptLogicEntity:GetPrivateScriptScope()
+
+	VRAperturePrint( "Created script logic entity" )
+
+	CallScriptLogicFunction( "OnInit" )
+
+	if vlua.contains( ScriptLogicScope, "OnUpdate" ) then
+		HasUpdateFunc = true
+	end
+end
+
+
+-----------------------------------------------------------
+function OnPrecache( context )
+	VRAperturePrint( "OnPrecache()" )
+
+	CallScriptLogicFunction( "OnPrecache", context )
+end
+
+
+-----------------------------------------------------------
+function OnActivate()
+	VRAperturePrint( "OnActivate()" )
+
+	CallScriptLogicFunction( "OnActivate" )
+end
+
+
+-----------------------------------------------------------
+function OnGameplayStart()
+	VRAperturePrint( "OnGameplayStart()" )
+
+	CallScriptLogicFunction( "OnGameplayStart" )
+end
+
+
+-----------------------------------------------------------
+function OnPlayerSpawned()
+	VRAperturePrint( "OnPlayerSpawned()" )
+
+	ScriptSystem_AddPerFrameUpdateFunction( OnUpdate )
+
+	CallScriptLogicFunction( "OnPlayerSpawned" )
+end
+
+
+-----------------------------------------------------------
+function OnUpdate()
+	if VRAperturePrint == false then
+		return
+	end
+
+	if HasUpdateFunc then
+		CallScriptLogicFunction( "OnUpdate" )
+	end
+end
+
+
+-----------------------------------------------------------
+function SpawnScriptEnt( entName, scriptName, pos, ang )
+	pos = pos or Vector(0, 0, 0)
+	ang = ang or QAngle(0, 0, 0)
+	local spawnTable = 
+	{
+		classname = "logic_script",
+		targetname = entName,
+		vscripts = scriptName,
+		origin = pos,
+		angles = ang
+	}
+	
+	local spawnedEnt = SpawnEntityFromTableSynchronous( "logic_script", spawnTable )
+	return spawnedEnt
+end
+
+
+-----------------------------------------------------------
+function CallScriptLogicFunction( functionName, params )
+	if vlua.contains( ScriptLogicScope, functionName ) then 
+		if params == nil then 
+			ScriptLogicScope[functionName]( ScriptLogicScope )
+		else 
+			ScriptLogicScope[functionName]( ScriptLogicScope, params )
+		end
+	else
+		VRAperturePrint( "Logic ent doesn't contain function named: " .. functionName )
+	end
+end
